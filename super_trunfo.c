f90d41831296532f827e6e391162ccdd65e42422
kawansantanan <kawansantana597@gmail.com>
2025-02-20 22:17:48 +0000

    segunda carta feita

--- super_trunfo.c
+++ super_trunfo.c
@@ -3,20 +3,40 @@
 int main(){
     printf("carta 1\n");
 
-    char estado = 'A';
-    char codigo[20] = "A01";
-    char nome[20] = "ilheus";
-    int populacao = 178649;
-    float area = 1.585;
-    float pib = 5.000000;
-    int turisticos = 10;
+    char estado1 = 'A';
+    char codigo1[20] = "A01";
+    char nome1[20] = "ilheus";
+    int populacao1 = 178649;
+    float area1 = 1.585;
+    float pib1 = 5.000000;
+    int turisticos1 = 10;
 
-    printf ("estado: %c\n", estado);
-    printf ("codigo: %s\n", codigo);
-    printf ("nome: %s\n", nome);
-    printf ("população: %d\n", populacao); 
-    printf ("área: %f\n", area);
-    printf ("pib: %f\n", pib);
-    printf ("pontos turísticos: %d\n", turisticos);
+    printf ("estado: %c\n", estado1);
+    printf ("codigo: %s\n", codigo1);
+    printf ("nome: %s\n", nome1);
+    printf ("população: %d\n", populacao1); 
+    printf ("área: %f\n", area1);
+    printf ("pib: %f\n", pib1);
+    printf ("pontos turísticos: %d\n", turisticos1);
+
+    printf ("\n");
+
+    printf("carta 2\n");
+
+    char estado2 = 'B';
+    char codigo2[20] = "B01";
+    char nome2[20] = "Joinville";
+    int populacao2 = 616.317;
+    float area2 = 1.126;
+    float pib2 = 45.000000000;
+    int turisticos2 = 20;
+
+    printf ("estado: %c\n", estado2);
+    printf ("codigo: %s\n", codigo2);
+    printf ("nome: %s\n", nome2);
+    printf ("população: %d\n", populacao2); 
+    printf ("área: %f\n", area2);
+    printf ("pib: %f\n", pib2);
+    printf ("pontos turísticos: %d\n", turisticos2);
 }
     
