57b2c0b19d98b59d365d6e3cf3d9fbe4b79f510a
kawansantanan <kawansantana597@gmail.com>
2025-02-20 21:59:25 +0000

    primeira carta feita

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 
-int main (){
-    printf("desafio super trunfo\n");
-    printf("novo commit\n");
-    
+int main(){
+    printf("carta 1\n");
 
+    char estado = 'A';
+    char codigo[20] = "A01";
+    char nome[20] = "ilheus";
+    int populacao = 178649;
+    float area = 1.585;
+    float pib = 5.000000;
+    int turisticos = 10;
 
-
-
-
-
-
-
-
-
+    printf ("estado: %c\n", estado);
+    printf ("codigo: %s\n", codigo);
+    printf ("nome: %s\n", nome);
+    printf ("população: %d\n", populacao); 
+    printf ("área: %f\n", area);
+    printf ("pib: %f\n", pib);
+    printf ("pontos turísticos: %d\n", turisticos);
 }
+    
