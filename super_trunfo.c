04bae238878c6532af62dfb3738c861b88a2bf86
kawansantanan <kawansantana597@gmail.com>
2025-02-24 19:29:31 +0000

    cartas feitas

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,41 +1,55 @@
 #include <stdio.h>
 
-int main (){
+int main(){
+    printf("carta 1\n");
 
-    char estado1 = 'A';
-    char codigo1[20] = "A01";
-    char nome1[20] = "ilheus";
-    int populacao1 = 178649;
-    float areaKm1 = 1.585;
-    float pib1 = 5.000000;
-    int pontosturisticos1 = 10;
+    char estado;
+    char codigo;
+    char nome;
+    int populacao;
+    float area;
+    float pib;
+    int pontosturisticos;
 
-    printf ("estado: %c\n", estado1);
-    printf ("codigo: %s\n", codigo1);
-    printf ("nome: %s\n", nome1);
-    printf ("população: %d\n", populacao1); 
-    printf ("área: %f\n", areaKm1);
-    printf ("pib: %f\n", pib1);
-    printf ("pontos turísticos: %d\n", pontosturisticos1);
 
-    printf ("\n");
+    printf ("estado: ");
+    scanf("%s", &estado);
+    printf ("código da carta: ");
+    scanf("%s", &codigo);
+    printf ("nome da cidade: ");
+    scanf("%s", &nome);
+    printf ("população: ");
+    scanf("%d", &populacao); 
+    printf ("área em Km: ");
+    scanf("%f", &area);
+    printf ("pib:");
+    scanf("%f", &pib);
+    printf ("pontos turísticos: ");
+    scanf("%d", &pontosturisticos);
+
+    printf("\n");
 
     printf("carta 2\n");
 
-    char estado2 = 'B';
-    char codigo2[20] = "B01";
-    char nome2[20] = "Joinville";
-    int populacao2 = 616317;
-    float areaKm2 = 1.126;
-    float pib2 = 45.000000000;
-    int pontosturisticos2 = 20;
-
-    printf ("estado: %c\n", estado2);
-    printf ("codigo: %s\n", codigo2);
-    printf ("nome: %s\n", nome2);
-    printf ("população: %d\n", populacao2); 
-    printf ("área: %f\n", areaKm2);
-    printf ("pib: %f\n", pib2);
-    printf ("pontos turísticos: %d\n", pontosturisticos2);
+    printf ("estado: ");
+    scanf("%s", &estado);
+    printf ("código da carta: ");
+    scanf("%s", &codigo);
+    printf ("nome da cidade: ");
+    scanf("%s", &nome);
+    printf ("população: ");
+    scanf("%d", &populacao); 
+    printf ("área em Km: ");
+    scanf("%f", &area);
+    printf ("pib:");
+    scanf("%f", &pib);
+    printf ("pontos turísticos: ");
+    scanf("%d", &pontosturisticos);
 }
+  
+    
+    
+
+
+    
     
