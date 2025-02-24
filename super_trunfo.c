e1654a09ff44e5198f56a56d7402f5f8d20d2ef3
kawansantanan <kawansantana597@gmail.com>
2025-02-24 19:25:58 +0000

    atualizações feitas

--- super_trunfo.c
+++ super_trunfo.c
@@ -6,17 +6,17 @@ int main (){
     char codigo1[20] = "A01";
     char nome1[20] = "ilheus";
     int populacao1 = 178649;
-    float area1 = 1.585;
+    float areaKm1 = 1.585;
     float pib1 = 5.000000;
-    int turisticos1 = 10;
+    int pontosturisticos1 = 10;
 
     printf ("estado: %c\n", estado1);
     printf ("codigo: %s\n", codigo1);
     printf ("nome: %s\n", nome1);
     printf ("população: %d\n", populacao1); 
-    printf ("área: %f\n", area1);
+    printf ("área: %f\n", areaKm1);
     printf ("pib: %f\n", pib1);
-    printf ("pontos turísticos: %d\n", turisticos1);
+    printf ("pontos turísticos: %d\n", pontosturisticos1);
 
     printf ("\n");
 
@@ -25,17 +25,17 @@ int main (){
     char estado2 = 'B';
     char codigo2[20] = "B01";
     char nome2[20] = "Joinville";
-    int populacao2 = 616.317;
-    float area2 = 1.126;
+    int populacao2 = 616317;
+    float areaKm2 = 1.126;
     float pib2 = 45.000000000;
-    int turisticos2 = 20;
+    int pontosturisticos2 = 20;
 
     printf ("estado: %c\n", estado2);
     printf ("codigo: %s\n", codigo2);
     printf ("nome: %s\n", nome2);
     printf ("população: %d\n", populacao2); 
-    printf ("área: %f\n", area2);
+    printf ("área: %f\n", areaKm2);
     printf ("pib: %f\n", pib2);
-    printf ("pontos turísticos: %d\n", turisticos2);
+    printf ("pontos turísticos: %d\n", pontosturisticos2);
 }
     
